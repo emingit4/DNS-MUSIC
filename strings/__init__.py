4f40de12cfd17f859433935eefd5bdc03513ade2
emingit4 <coniuk7@gmail.com>
2025-01-25 19:00:39 +0400

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -48,7 +48,7 @@ for filename in os.listdir(r"./strings/langs/"):
         languages[language_name] = yaml.safe_load(
             open(r"./strings/langs/" + filename, encoding="utf8")
         )
-        for item in languages["tr"]:
+        for item in languages["tuk"]:
             if item not in languages[language_name]:
                 languages[language_name][item] = languages["tuk"][item]
     try:
