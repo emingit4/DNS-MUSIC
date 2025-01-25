5a75d161458bbd99ee42c598a53d31c69cf9c7da
emingit4 <coniuk7@gmail.com>
2025-01-25 21:20:25 +0400

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -1,30 +1,60 @@
+#
+# Copyright (C) 2024 by MISH0009@Github, < https://github.com/MISH0009 >.
+#
+# This file is part of < https://github.com/MISH0009/DNS > project,
+# and is released under the MIT License.
+# Please see < https://github.com/MISH0009/DNS/blob/master/LICENSE >
+#
+# All rights reserved
+
 import os
 import sys
 from typing import List
+
 import yaml
 
 languages = {}
 commands = {}
+
 languages_present = {}
+
 
 def get_command(value: str) -> List:
     return commands["command"][value]
 
+
 def get_string(lang: str):
     return languages[lang]
 
-# Türkçe dilini yükləyirik (hər dəfə bot başladıqda)
-def load_turkish_language():
+
+for filename in os.listdir(r"./strings"):
+    if filename.endswith(".yml"):
+        language_name = filename[:-4]
+        commands[language_name] = yaml.safe_load(
+            open(r"./strings/" + filename, encoding="utf8")
+        )
+
+
+for filename in os.listdir(r"./strings/langs/"):
+    if "en" not in languages:
+        languages["en"] = yaml.safe_load(
+            open(r"./strings/langs/en.yml", encoding="utf8")
+        )
+        languages_present["en"] = languages["en"]["name"]
+    if filename.endswith(".yml"):
+        language_name = filename[:-4]
+        if language_name == "en":
+            continue
+        languages[language_name] = yaml.safe_load(
+            open(r"./strings/langs/" + filename, encoding="utf8")
+        )
+        for item in languages["en"]:
+            if item not in languages[language_name]:
+                languages[language_name][item] = languages["en"][item]
     try:
-        with open(r"./strings/langs/tuk.yml", encoding="utf8") as file:
-            languages["tuk"] = yaml.safe_load(file)
-        languages_present["tuk"] = languages["tuk"]["name"]
-        print(f"Türkçe dil faylı uğurla yükləndi: {languages['tuk'].get('name', 'Ad tapılmadı')}")
-    except Exception as e:
-        print(f"Türkçe dil faylı yüklənərkən xəta baş verdi: {str(e)}")
+        languages_present[language_name] = languages[language_name]["name"]
+    except:
+        print(
+            "There is some issue with the language file inside bot. Please report it to the TheTeamvk at @TheTeamvk on Telegram"
+        )
         sys.exit()
-
-# Türkçe dilini yükləyirik
-load_turkish_language()
-
-# Bot başladıqda dilin doğru yükləndiyini yoxlayırıq
