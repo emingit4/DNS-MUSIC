9e2f35eccac1fd78a719b68ea0b261252c74b6a8
emingit4 <coniuk7@gmail.com>
2025-01-25 20:19:18 +0400

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -1,60 +1,42 @@
-#
-# Copyright (C) 2024 by MISH0009@Github, < https://github.com/MISH0009 >.
-#
-# This file is part of < https://github.com/MISH0009/DNS > project,
-# and is released under the MIT License.
-# Please see < https://github.com/MISH0009/DNS/blob/master/LICENSE >
-#
-# All rights reserved
-
 import os
 import sys
 from typing import List
-
 import yaml
 
 languages = {}
 commands = {}
-
 languages_present = {}
-
 
 def get_command(value: str) -> List:
     return commands["command"][value]
 
-
-def get_string(lang: str = "tuk"):
+def get_string(lang: str):
     return languages[lang]
 
-
-for filename in os.listdir(r"./strings"):
-    if filename.endswith(".yml"):
-        language_name = filename[:-4]
-        commands[language_name] = yaml.safe_load(
-            open(r"./strings/" + filename, encoding="utf8")
-        )
-
-
+# Türkçəni əvvəlcə yükləyirik
 for filename in os.listdir(r"./strings/langs/"):
-    if "tuk" not in languages:
-        languages["tuk"] = yaml.safe_load(
-            open(r"./strings/langs/tuk.yml", encoding="utf8")
-        )
-        languages_present["tuk"] = languages["tuk"]["name"]
     if filename.endswith(".yml"):
         language_name = filename[:-4]
-        if language_name == "tuk":
-            continue
-        languages[language_name] = yaml.safe_load(
-            open(r"./strings/langs/" + filename, encoding="utf8")
-        )
-        for item in languages["tuk"]:
-            if item not in languages[language_name]:
-                languages[language_name][item] = languages["tuk"][item]
-    try:
-        languages_present[language_name] = languages[language_name]["name"]
-    except:
-        print(
-            "There is some issue with the language file inside bot. Please report it to the TheTeamvk at @TheTeamvk on Telegram"
-        )
-        sys.exit()
+        if language_name == "tr":  # Türkçəni yükləyirik
+            languages["tr"] = yaml.safe_load(
+                open(r"./strings/langs/tr.yml", encoding="utf8")
+            )
+            languages_present["tr"] = languages["tr"]["name"]
+        else:
+            # Digər dilləri yükləyirik, amma Türkçəni əsas olaraq saxlayırıq
+            languages[language_name] = yaml.safe_load(
+                open(r"./strings/langs/" + filename, encoding="utf8")
+            )
+            # Türkçədən məlumatı əlavə edirik
+            for item in languages["tr"]:
+                if item not in languages[language_name]:
+                    languages[language_name][item] = languages["tr"][item]
+        try:
+            languages_present[language_name] = languages[language_name]["name"]
+        except:
+            print("Dil faylında problem var. Zəhmət olmasa, problemi @TheTeamvk-ya bildirin.")
+            sys.exit()
+
+# Başlanğıcda Türkçə dilini seçirik
+default_language = "tr"
+print(f"Bot başladı, dil: {languages_present.get(default_language)}")
