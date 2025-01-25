878b9e31331bdc1cd8fd8ad291d8eb432101a025
emingit4 <coniuk7@gmail.com>
2025-01-25 20:27:17 +0400

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -21,6 +21,7 @@ for filename in os.listdir(r"./strings/langs/"):
         # Türkçəni ilk olaraq yükləyirik
         if language_name == "tr":  
             try:
+                # Türkçə faylını yükləməyə çalışırıq
                 languages["tr"] = yaml.safe_load(open(r"./strings/langs/tr.yml", encoding="utf8"))
                 languages_present["tr"] = languages["tr"]["name"]
                 print("Türkçe dil faylı uğurla yükləndi.")  # Debug mesajı
@@ -28,23 +29,23 @@ for filename in os.listdir(r"./strings/langs/"):
                 print(f"Türkçe dil faylı yüklənərkən xəta baş verdi: {e}")
                 sys.exit()
         else:
+            # Digər dilləri yükləyirik
             languages[language_name] = yaml.safe_load(open(r"./strings/langs/" + filename, encoding="utf8"))
+            # Türkçədən məlumatları əlavə edirik
             for item in languages["tr"]:
                 if item not in languages[language_name]:
                     languages[language_name][item] = languages["tr"][item]
-        
+
         try:
             languages_present[language_name] = languages[language_name]["name"]
         except KeyError:
             print(f"Error loading language file: {language_name}. Please check the language file format.")
             sys.exit()
 
-# languages sözlüyünü çap edirik
-print("Languages:", languages)
+# Yüklənmiş dilləri yoxlayaq
+print("Languages loaded:", languages)
 
-# Başlanğıcda Türkçe dilini seçirik
-default_language = "tr"
-if default_language in languages:
-    print(f"Bot başladı, dil: {languages_present.get(default_language)}")
-else:
-    print("Türkçe dili tapılmadı. Dil faylını yoxlayın.")
+# Əgər Türkçe yüklənməyibsə, Xəta mesajı verək
+if "tr" not in languages:
+    print("Türkçe dili tapılmadı. Dil faylının doğru yükləndiyini yoxlayın!")
+    sys.exit()
